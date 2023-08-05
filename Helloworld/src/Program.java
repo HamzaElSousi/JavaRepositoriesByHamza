11140261bbe6c69350f41f94aca90c9642d4657e
HamzaElSousi <hamza.sousi1998@gmail.com>
2023-08-04 23:13:12 -0400

    helloWorld
    I'm learning all about github now and practicing by uploading my projects to ease the learning curve. 
    First Lab in first semester (winter_2023). 
    If you see something you can correct me with, let me know. I highly appreciate it.

---
+++ Helloworld/src/Program.java
@@ -0,0 +1,20 @@
+/*
+* Student Name: Hamza El Sousi
+* Lab Professor: Professor Teddy Yap
+* Due Date: Jan 27th ,2023
+* Modified: Jan 18th 
+* Description: Lab exercise 1 demo
+*/
+/* This class represents a typical first program, Hello World. */
+public class Program {
+
+	public static void main(String[] args) {
+		String name = "Hamza El Sousi"; // I input my name 
+		System.out.println("Welcome to 23W CST8116!"); // this is a welcome message to welcome new student
+		System.out.print("Java says: Hello ");
+		System.out.print(name);
+		System.out.println(); // new-line
+		System.out.println("Program modified by " + name);
+	}
+	
+}
